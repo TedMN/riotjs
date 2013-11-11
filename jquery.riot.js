1d81965c3c1c0aff8dfd2e4f78f3eb7f6143062d
TedMN <johnsontedm@gmail.com>
2013-11-11 17:43:46 +0000

    Update jquery.riot.js
    consistent string quotes.  JS should use single quotes and HTML double.

--- jquery.riot.js
+++ jquery.riot.js
@@ -1,20 +1,21 @@
 /* Riot.js 0.9.4 | moot.it/riotjs | @license MIT | (c) 2013 Tero Piirainen, Moot Inc, other contributors */
 (function(top) {
-  "use strict";
+  'use strict';
 
   var $ = top.$, // jQuery or Zepto
       pop_event_name = 'popstate',
       empty_string = '',
-      slice = [].slice; // A classic pattern for separating concerns
-      
+      slice = [].slice, // A classic pattern for separating concerns
+      $win = $(top); // jQueried window object
+
   // avoid multiple execution. popstate should be fired only once etc.
   if ($.riot) return;
 
-  $.riot = "0.9.4";
+  $.riot = '0.9.4';
 
   $.observable = function(obj) {
 
-    var $el = $("<a/>"); // plain object not working on Zepto
+    var $el = $('<a/>'); // plain object not working on Zepto
 
     $.each(['on', 'one', 'trigger', 'off'], function(i, name) {
       obj[name] = function(names, fn) {
@@ -23,7 +24,7 @@
         if (i < 2) {
           $el[name](names, function(e) {
             var args = slice.call(arguments, 1);
-            if (names.split(" ")[1]) args.unshift(e.type);
+            if (names.split(' ')[1]) args.unshift(e.type);
             fn.apply(obj, args);
           });
 
@@ -61,16 +62,15 @@
 
   };
 
-  // jQueried window object
-  var $win = $(top);
-
   // emit window.popstate event consistently on page load, on every browser
   var page_popped;
 
-  $win.on("load", function(e) {
+  $win.on('load', function(e) {
+    //Must avoid timeout
     top.setTimeout(function() { page_popped || $win.trigger(pop_event_name); }, 1);
 
   }).on(pop_event_name, function(e) {
+    //Called all the time, not ideal, plus not leveraging jQuery
     if (!page_popped) page_popped = true;
 
   });
