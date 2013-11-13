916084f85eb2b935edd82f99eaf384b640d01ecb
Ted Johnson <johnsontedm@gmail.com>
2013-11-13 07:42:14 +0000

    Example of $.render improvement options/ideas.

--- ext/render.js
+++ ext/render.js
@@ -1,22 +1,35 @@
-/*
-  Riot.js templating 0.9.4 | moot.it/riotjs | @license MIT
-  (c) 2013 Tero Piirainen, Moot Inc and other contributors.
- */
-(function() {
+/* Riot.js templating 0.9.4 | moot.it/riotjs | @license MIT | (c) 2013 Tero Piirainen, Moot Inc and other contributors. */
+(function(top) {
+	
+  var $ = top.$,
+  	  templates = {},  //Store templates for reference by name
+  	  regEx = /\{\s*(\w+)\s*\}/g,
+  	  compile = function (template) {
+	  	//Here one could - Strip out <script...> ... </script> and strip out eval() if in events like on click.
+  	  	return function(data){
+  	  	  return typeof data === '[object]' && template.replace(regEx, function(match) { return data[match] ? data[match] : ''; });
+  	  	};
+      };
 
-  // Precompiled templates (JavaScript functions)
-  var FN = {};
-
-  // Render a template with data
-  $.render = function(template, data) {
-    return !template ? '' : (FN[template] = FN[template] || new Function("_",
-      "return '" + template
-        .replace(/\n/g, "\\n")
-        .replace(/\r/g, "\\r")
-        .replace(/'/g, "\\'")
-        .replace(/\{\s*(\w+)\s*\}/g, "' + (_.$1 === undefined || _.$1 === null ? '' : _.$1) + '") +
-      "'"
-    ))(data);
-  }
-
-})()
+  /* Render, store a template
+   * This approach was needed as some browsers do not allow eval oriented calls: http://developer.chrome.com/extensions/contentSecurityPolicy.html
+   * Provide a parameters object, 
+   * { 
+   *  name: 'the name to reference to access or store a template', 
+   *  template: 'the template to store and/or depending on if data and a name is provided', 
+   *  data: 'the data to use on the template provided or looked up by name'
+   * } 	 
+   */
+  $.render = function(params) {
+	var params = params || {}, name = params.name, template = params.template, data = params.data, t = null;
+	  
+	if(template) {
+	  templates[name] = compiled = compile(template);
+	} else if (name) {
+	  compiled = templates[name];
+	}
+	  
+	return compiled && compiled(data);
+  };
+    
+})(window);
